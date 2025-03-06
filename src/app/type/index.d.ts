f1c960278886c7bec4bafed3dbd3602fa81c669d
nguyennam1029 <namcoi1029@gmail.com>
2025-03-06 21:12:52 +0700

    web hook

--- src/app/type/index.d.ts
+++ src/app/type/index.d.ts
@@ -8,7 +8,12 @@ type TMenuItem = {
   title: string;
   icon: React.ReactNode;
 }
-export {
-    TActiveLinksProps,
-    TMenuItem
-}
+// User
+type TCreateUserParams = {
+  clerkId: string;
+  username: string;
+  email_address: string;
+  name?: string;
+  avatar?: string;
+};
+export { ActiveLinkProps, TCreateUserParams, TMenuItem };
